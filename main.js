c526e98da01da2c8583af39d24047dcd589b05e5
jordan <jleejx2000@gmail.com>
2021-07-20 13:09:43 +0800

    rendered gameBoard

---
+++ main.js
@@ -0,0 +1,10 @@
+const gameBoard = (() => {
+  const board = Array.from(document.querySelector("#gameBoard").children);
+  console.log(board);
+})();
+
+const displayController = (() => {})();
+
+const playerFactory = () => {
+  return {};
+};
