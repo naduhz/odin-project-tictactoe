27e673dd5c28c7761cf14ae93839687b4e8c0b29
jordan <jleejx2000@gmail.com>
2021-07-21 09:09:45 +0800

    event listener for markers

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
+const CROSS = "\u2715";
+const CIRCLE = "\u25EF";
+let playerMarker = "";
+
+const displayController = (() => {
+  const crossMark = document.querySelector("#cross-mark");
+  const circleMark = document.querySelector("#circle-mark");
+
+  crossMark.addEventListener("click", () => {
+    crossMark.classList.add("buttonPressed");
+    circleMark.className = "button";
+    playerMarker = CROSS;
+  });
+
+  circleMark.addEventListener("click", () => {
+    circleMark.classList.add("buttonPressed");
+    crossMark.className = "button";
+    playerMarker = CIRCLE;
+  });
+})();
+
 const gameBoard = (() => {
   const board = Array.from(document.querySelector("#gameBoard").children);
   board.forEach((square) => {
     square.addEventListener("click", () => {
       if (!square.textContent) {
-        square.textContent = "\u25EF";
+        square.textContent = playerMarker;
       } else return;
     });
   });
 })();
-
-const displayController = (() => {})();
 
 const playerFactory = () => {
   return {};
